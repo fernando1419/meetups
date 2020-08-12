7b1fe5fc6a341632c85944d31ac2c537909dbc3f
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-12 18:23:38 -0300

    Added app toolbars and styling them

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,28 @@ module.exports = {
     node: true
   },
   extends: [
+    "eslint:recommended",
     "plugin:vue/recommended",
-    "eslint:recommended"
-    // "@vue/typescript/recommended",
-    // "plugin:vue/strongly-recommended"
+    "@vue/typescript/recommended",
     // "prettier",
     // "prettier/vue"
+    // "plugin:vue/strongly-recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
+    // custom rules:
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "normal": "never", // HTML elements. E.g. <div></div>
+        "void": "always", // HTML void elements. E.g. <img>
+        // "components": "always", // Vue.js components.
+      },
+      "svg": "always",
+      "math": "always",
+    }],
+
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,84 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="showSideBar"
+      temporary
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer />
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-list
+        dense
+        nav
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-main>
-      <HelloWorld />
-    </v-main>
+        <v-divider />
+
+        <!-- <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Application
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              subtext
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item> -->
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon> supervisor_account </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title> View Meetups </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <div>
+      <v-toolbar
+        dense
+        dark
+        class="purple darken-1"
+      >
+        <v-app-bar-nav-icon
+          class="hidden-sm-and-up"
+          @click="showSideBar = !showSideBar"
+        />
+        <v-toolbar-title>Meetups</v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items class="hidden-xs-only">
+          <v-icon left>
+            supervisor_account
+          </v-icon>
+          <v-btn text>
+            View Meetups
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+
+
+    <main></main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
 
 export default Vue.extend({
   name: 'App',
-
   components: {
-    HelloWorld
   },
-
   data: () => ({
-    //
+    showSideBar: false
   })
 })
 </script>
