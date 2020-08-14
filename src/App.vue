00dab04287e3447908050bd0395264c2f2c3bf85
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-13 23:00:36 -0300

    Added main components and routes for the app

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,15 @@
       app
     >
       <v-list
-        v-for="item in menuItems"
-        :key="item.title"
         dense
         nav
       >
-        <!-- <v-list-item>
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/78.jpg" />
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>John Leider</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item> -->
-
-        <!-- <v-divider /> -->
-
-        <!-- <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title">
-              Application
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              subtext
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item> -->
-
-        <v-list-item link>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+          link
+        >
           <v-list-item-icon>
             <v-icon> {{ item.icon }} </v-icon>
           </v-list-item-icon>
@@ -54,25 +35,37 @@
           class="hidden-sm-and-up"
           @click="showSideBar = !showSideBar"
         />
-        <v-toolbar-title>Meetups</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link
+            to="/"
+            tag="span"
+            style="cursor: pointer"
+          >
+            Meetups
+          </router-link>
+        </v-toolbar-title>
         <v-spacer />
         <v-toolbar-items
-          v-for="item in menuItems"
-          :key="item.title"
           class="hidden-xs-only"
         >
-          <v-icon left>
-            {{ item.icon }}
-          </v-icon>
-          <v-btn text>
+          <v-btn
+            v-for="item in menuItems"
+            :key="item.title"
+            text
+            :to="item.link"
+          >
+            <v-icon left>
+              {{ item.icon }}
+            </v-icon>
             {{ item.title }}
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </div>
 
-
-    <main></main>
+    <main>
+      <router-view />
+    </main>
   </v-app>
 </template>
 
@@ -86,11 +79,11 @@ export default Vue.extend({
   data: () => ({
     showSideBar: false,
     menuItems: [
-      { icon: 'supervisor_account', title: 'View Meetups'},
-      { icon: 'room', title: 'Organize Meetup' },
-      { icon: 'person', title: 'Profile' },
-      { icon: 'face', title: 'Sign up' },
-      { icon: 'lock_open', title: 'Sign in' }
+      { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+      { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+      { icon: 'person', title: 'Profile', link: '/profile' },
+      { icon: 'face', title: 'Sign up', link: '/signup' },
+      { icon: 'lock_open', title: 'Sign in', link: '/signin' }
     ]
   })
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,61 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "@/components/Home.vue";
+import Meetups from "@/components/Meetup/Meetups.vue";
+import CreateMeetup from "@/components/Meetup/CreateMeetup.vue";
+import Profile from "@/components/User/Profile.vue";
+import Signup from "@/components/User/Signup.vue";
+import Signin from "@/components/User/Signin.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/meetups",
+    name: "Meetups",
+    component: Meetups
+  },
+  {
+    path: "/meetup/new",
+    name: "CreateMeetup",
+    component: CreateMeetup
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: Signin
   }
-]
+
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
