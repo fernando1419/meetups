17a2aeaec4f719f830eb296a59dc4a82b30c83bc
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-16 18:31:15 -0300

    Added a carousel component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,108 @@
 <template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+  <v-container>
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs12
+        sm6
+        class="text-xs-center text-sm-right"
+      >
+        <v-btn
+          large
+          to="/meetups"
+          class="primary"
+        >
+          Explore Meetups
+        </v-btn>
+      </v-flex>
+      <v-flex
+        xs12
+        sm6
+        class="text-xs-center text-sm-left"
+      >
+        <v-btn
+          large
+          to="/meetup/new"
+          class="primary"
+        >
+          Organize Meetup
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout
+      row
+      wrap
+      class="mt-2"
+    >
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :key="meetup.id"
+            :src="meetup.imageUrl"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout
+      row
+      wrap
+      class="mt-2"
+    >
+      <v-flex
+        xs12
+        class="align-center justify-center d-flex"
+      >
+        <p>Join our meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      meetups: [
+        {
+          id: 'alksdjfaldfj',
+          title: 'Meetup in New York',
+          imageUrl: 'https://www.brinksresiliencetraining.com/wp-content/uploads/2019/10/New-York-City.jpg'
+        },
+        {
+          id: 'alksdjfaldfjasdfasfd',
+          title: 'Meetup in Paris',
+          imageUrl: 'https://es.oui.sncf/assets/vocabularies/locality/visuals/Paris_destination_1.jpg'
+        },
+        {
+          id: 'alksdjfaldfjasdfaasdfd',
+          title: 'Meetup in Russia',
+          imageUrl: 'https://static3lonelyplanetes.cdnstatics.com/sites/default/files/styles/max_1300x1300/public/blog/rusia_moscu_puertaresurreccion_plazaroja_shutterstockrf_493315507_marco_rubino_shutterstock.jpg?itok=wXAriIgg'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .title {
+    /* position: relative; */
+    /* bottom: 50px; */
+    background-color: rgba(0, 0, 0, 0.3);
+    color:white;
+    font-size: 2em;
+    /* padding: 10px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
